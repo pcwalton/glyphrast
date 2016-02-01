7d2f7510399a651df7ffc97fc5154d491f14eb0b
Patrick Walton <pcwalton@mimiga.net>
2016-02-01 09:38:50 -0800

    WIP box AA, part 1

--- glyph.vs.glsl
+++ glyph.vs.glsl
@@ -13,18 +13,25 @@ in vec2 aRasterOrigin;
 in float aRasterHeight;
 in int aCurveCount;
 
-float RasterX(vec2 p0, vec2 p1, vec2 p3, float glyphY) {
-    if (p0.y != p3.y && glyphY >= min(p0.y, p3.y) && glyphY <= max(p0.y, p3.y)) {
+bool IntersectsCurve(vec2 p0, vec2 p3, float y) {
+    return y >= min(p0.y, p3.y) && y <= max(p0.y, p3.y);
+}
+
+// Returns the bottom and top X extents.
+float RasterX(vec2 p0, vec2 p1, vec2 p3, float glyphYAAExtents) {
+    if (p0.y != p3.y && IntersectsCurve(p0, p3, glyphYAAExtents)) { /*&&
+            IntersectsCurve(p0, p3, glyphYAAExtents.y)) {*/
         // https://www.reddit.com/r/MathHelp/comments/3pt8l5/
         //  quadratic_bezier_curve_line_intersections_the/
         //
         // TODO(pcwalton): Algebraic simplifications. Don't trust the driver to do it for us.
         float a = (p0.y - p1.y) + (p3.y - p1.y);
         float b = -2.0 * (p0.y - p1.y);
-        float c = p0.y - glyphY;
+        float c = p0.y - glyphYAAExtents;
 
-        float t0 = (-b + sqrt(b * b - 4.0 * a * c)) / (2.0 * a);
-        float t1 = (-b - sqrt(b * b - 4.0 * a * c)) / (2.0 * a);
+        float discriminant = sqrt((b * b) - 4.0 * (a) * c);
+        float t0 = (-b + discriminant) / (2.0 * a);
+        float t1 = (-b - discriminant) / (2.0 * a);
         float t = t0 >= 0.0 && t0 <= 1.0 ? t0 : t1;
 
         float oneMinusT = 1.0 - t;
@@ -40,8 +47,10 @@ bool Winding(vec2 p0, vec2 p3) {
     return (p0.y == p3.y) ? (p0.x <= p3.x) : (p0.y < p3.y);
 }
 
-float Encode(float rasterX, float ySubpixel, bool winding) {
-    return round(rasterX * 16.0) * 32.0 + ySubpixel * 2.0 + (winding ? 1.0 : 0.0);
+float Encode(float rasterX, bool winding) {
+    float spread = rasterX.y - rasterX.x;
+    float encodedSpread = round((clamp(-2.0, 2.0, spread) + 2.0) * 8.0);
+    return round(rasterX * 16.0) * 32.0 + encodedSpread + (winding ? 1.0 : 0.0);
 }
 
 void main() {
@@ -50,6 +59,7 @@ void main() {
     float rasterHeight = aRasterHeight;
 
     float glyphY = mod(y, rasterHeight);
+    vec2 glyphYAAExtents = vec2(glyphY - 0.0, glyphY + 0.0);
     float rasterY = y + aRasterOrigin.y;
     float scaleFactor = rasterHeight / glyphHeight;
     vec2 ap0 = aAP0 * scaleFactor;
@@ -61,8 +71,8 @@ void main() {
     vec2 bp2 = aBP2 * scaleFactor;
     vec2 bp3 = aBP3 * scaleFactor;
 
-    float encodedRasterAX = Encode(RasterX(ap0, ap1, ap3, glyphY), 15.0, Winding(ap0, ap3));
-    float encodedRasterBX = Encode(RasterX(bp0, bp1, bp3, glyphY), 15.0, Winding(bp0, bp3));
+    float encodedRasterAX = Encode(RasterX(ap0, ap1, ap3, glyphYAAExtents.x), Winding(ap0, ap3));
+    float encodedRasterBX = Encode(RasterX(bp0, bp1, bp3, glyphYAAExtents.x), Winding(bp0, bp3));
     gl_Position = vec4(encodedRasterAX, rasterY, encodedRasterBX, 1.0);
 }
 
